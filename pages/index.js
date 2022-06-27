5948e7fee2312d5fb9f2822d7cb8950712d9dff4
Grady Kisibu <kisibugrady3980@gmail.com>
2022-06-27 14:10:07 +0200

    done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import styles from '../styles/Home.module.css'
-import Textcontainer from './Textcontainer'
 
 export default function Home() {
   return (
     <div>
-      <Textcontainer/>
+     
      
     </div>
   )
